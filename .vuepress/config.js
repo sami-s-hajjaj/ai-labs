b2a7ef9df0446831661e944c8ac7656e4698da86
Richard Wong <ricwtk@gmail.com>
2019-08-14 11:56:49 +0800

    first commit

---
+++ .vuepress/config.js
@@ -0,0 +1,40 @@
+module.exports = {
+  title: "CSC3206",
+  base: "/ai-labs/",
+  head: [
+    ["link", { rel: "stylesheet", href: "https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900" }],
+    ["link", { rel: "stylesheet", href: "https://cdn.jsdelivr.net/npm/@mdi/font@3.x/css/materialdesignicons.min.css" }],
+    ["link", { rel: "stylesheet", href: "https://cdn.jsdelivr.net/npm/vuetify@2.x/dist/vuetify.min.css" }],
+  ],
+  themeConfig: {
+    nav: [
+      { text: 'AI', link: '/ai' },
+      { text: 'CI', link: '/ci' },
+    ],
+    sidebarDepth: 2,
+    sidebar: [
+      '/',
+      {
+        title: '2019 August',
+        collapsable: false,
+        children: [
+          '/lab1',
+          '/lab2',
+          '/lab3',
+          '/lab4',
+          '/lab5',
+          '/lab6',
+          '/lab7',
+          '/lab8'
+        ]
+      }
+    ],
+    lastUpdated: 'Last Updated'
+  },
+  markdown: {
+    lineNumbers: true
+  },
+  plugins: [
+    'clean-urls'
+  ]
+}
