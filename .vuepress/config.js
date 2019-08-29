8b413715117c66c8e5e89b210a5d39444595ef61
Richard <ricwtk@gmail.com>
2019-08-29 19:06:30 +0800

    update lab2

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -21,7 +21,7 @@ module.exports = {
           '/lab1',
           '/lab2',
           '/lab3',
-          // '/lab4',
+          '/lab4',
           // '/lab5',
           // '/lab6',
           // '/lab7'
