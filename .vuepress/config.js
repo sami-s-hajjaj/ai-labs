7d3078fe9a241b4439514e2948d14fce784d7ca4
Richard Wong <ricwtk@gmail.com>
2019-09-24 09:45:58 +0800

    update side menu

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -22,7 +22,7 @@ module.exports = {
           '/lab2',
           '/lab3',
           '/lab4',
-          // '/lab5',
+          '/lab5',
           // '/lab6',
           // '/lab7',
           // '/lab8'
